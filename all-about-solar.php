e56d53639a407b4da4c89c2df54d6740c0c79499
saurbhcoder <saurbhcoder@gmail.com>
2026-02-18 10:27:08 +0530

    all

--- all-about-solar.php
+++ all-about-solar.php
@@ -1,3 +1,9 @@
 <?php include "header.php"; ?>
-
+<div class="subheader">
+        <div class="container">
+            <div class="subheader-inner">
+                <h2 class="subheader-title">All About Solar</h2>
+            </div>
+        </div>
+    </div>
 <?php include "footer.php"; ?>
