1b0f1a56eb262e4abe117038486669de0ee775e9
saurbhcoder <saurbhcoder@gmail.com>
2026-03-11 16:32:39 +0530

    k

--- submitpop.php
+++ submitpop.php
@@ -6,22 +6,11 @@ ini_set('display_errors', 1);
 
 
 
-$to = "customersupport@corvelsolar.com";
-$subject = "New Solar Enquiry";
-$message = "New Solar Lead\n\n";
-
-$headers = "From: customersupport@corvelsolar.com\r\n";
-$headers .= "Reply-To: customersupport@corvelsolar.com\r\n";
-$headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
-
-
-
-if(mail($to,$subject,$message,$headers)){
-    echo 'y';
+if(mail("customersupport@corvelsolar.com","Test","Mail Test")){
+echo "Mail working";
+}else{
+echo "Mail not working";
 }
-else
-    {
-        echo "Mail Failed<br>";
-    var_dump(error_get_last());
-    }
+
+
 ?>
