717d75fba39577376320ae93f481bc3f17a39b73
saurbhcoder <saurbhcoder@gmail.com>
2026-03-11 16:25:31 +0530

    send

--- submitpop.php
+++ submitpop.php
@@ -1,51 +1,19 @@
 <?php
+$to = "customersupport@corvelsolar.com";
+$subject = "New Solar Enquiry";
+$message = "New Solar Lead\n\n";
 
-use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
+$headers = "From: customersupport@corvelsolar.com\r\n";
+$headers .= "Reply-To: customersupport@corvelsolar.com\r\n";
+$headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-require 'PHPMailer/Exception.php';
-require 'PHPMailer/PHPMailer.php';
-require 'PHPMailer/SMTP.php';
 
-if(isset($_POST['name'])){
 
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$pincode = $_POST['pincode'];
-$bill = $_POST['bill'];
-
-$mail = new PHPMailer(true);
-//Support!@#12
-try {
-
-$mail->isSMTP();
-$mail->Host       = 'smtp.hostinger.com';
-$mail->SMTPAuth   = true;
-$mail->Username   = 'customersupport@corvelsolar.com';
-$mail->Password   = 'Support!@#12';
-$mail->SMTPSecure = 'tls';
-$mail->Port       = 587;
-
-$mail->setFrom('customersupport@corvelsolar.com', 'Corvel Solar');
-$mail->addAddress('customersupport@corvelsolar.com');
-
-$mail->Subject = 'New Solar Enquiry';
-
-$mail->Body = "xxx";
-
-$mail->send();
-
-header("Location: thank-you.php");
-exit();
-
-} catch (Exception $e) {
-
-echo "Mailer Error: " . $mail->ErrorInfo;
-
+if(mail($to,$subject,$message,$headers)){
+    echo 'y';
 }
-
-}else{
-    echo 'zzz';
-}
-
+else
+    {
+         echo 'n';
+    }
 ?>
