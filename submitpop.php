28e9ab31f6902f8804b406e99f67305059c33481
saurbhcoder <saurbhcoder@gmail.com>
2026-03-11 16:26:31 +0530

    err

--- submitpop.php
+++ submitpop.php
@@ -1,4 +1,11 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
+
+
 $to = "customersupport@corvelsolar.com";
 $subject = "New Solar Enquiry";
 $message = "New Solar Lead\n\n";
@@ -14,6 +21,7 @@ if(mail($to,$subject,$message,$headers)){
 }
 else
     {
-         echo 'n';
+        echo "Mail Failed<br>";
+    var_dump(error_get_last());
     }
 ?>
