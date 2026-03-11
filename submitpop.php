52c7aa9a889ac193d83f37d7213f88b25b59c4a8
saurbhcoder <saurbhcoder@gmail.com>
2026-03-11 14:59:50 +0530

    test

--- submitpop.php
+++ submitpop.php
@@ -1,26 +1,45 @@
 <?php
 
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$pincode = $_POST['pincode'];
-$bill = $_POST['bill'];
+// $name = $_POST['name'];
+// $phone = $_POST['phone'];
+// $pincode = $_POST['pincode'];
+// $bill = $_POST['bill'];
+
+// $to = "customersupport@corvelsolar.com";
+// $subject = "New Solar Enquiry";
+
+// $message = "
+// Name: $name
+// Phone: $phone
+// Pincode: $pincode
+// Electricity Bill: $bill
+// ";
+
+// $headers = "From: customersupport@corvelsolar.com";
+
+// mail($to,$subject,$message,$headers);
+
+// header("Location: thank-you.php");
+// exit();
+
+
+
+
+
+?>
+
+
+<?php
 
 $to = "customersupport@corvelsolar.com";
-$subject = "New Solar Enquiry";
-
-$message = "
-Name: $name
-Phone: $phone
-Pincode: $pincode
-Electricity Bill: $bill
-";
-
+$subject = "Mail Test";
+$message = "Testing PHP mail function";
 $headers = "From: customersupport@corvelsolar.com";
 
-mail($to,$subject,$message,$headers);
-
-header("Location: thank-you.php");
-exit();
-
+if(mail($to,$subject,$message,$headers)){
+    echo "Mail Sent Successfully";
+}else{
+    echo "Mail Failed";
+}
 
 ?>
