2ac524673f70e9a335ce45812209afe0759b9149
saurbhcoder <saurbhcoder@gmail.com>
2026-03-12 09:50:06 +0530

    c

--- submitpop.php
+++ submitpop.php
@@ -1,34 +1,18 @@
 <?php
 
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-
-
-echo "SMTP: ".ini_get("SMTP")."<br>";
-echo "SMTP PORT: ".ini_get("smtp_port")."<br>";
-echo "sendmail_path: ".ini_get("sendmail_path");
-
-
-
-
-$headers = "From: Corvel Solar <customersupport@corvelsolar.com>\r\n";
-$headers .= "Reply-To: customersupport@corvelsolar.com\r\n";
-$headers .= "Return-Path: customersupport@corvelsolar.com\r\n";
-$headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
-$headers .= "X-Mailer: PHP/" . phpversion();
+ini_set('display_errors',1);
 
 $to = "customersupport@corvelsolar.com";
-$subject = "Test Mail";
-$message = "This is test email from website.";
 
-$headers = "From: customersupport@corvelsolar.com";
+$subject = "Hostinger Mail Test";
 
-if(mail($to,$subject,$message,$headers)){
-    echo "Mail Sent";
-}else{
-    echo "Mail Failed";
-}
+$message = "Testing email from server";
 
+$headers = "From: customersupport@corvelsolar.com\r\n";
+
+$result = mail($to,$subject,$message,$headers);
+
+var_dump($result);
 
 ?>
