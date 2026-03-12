605e29d241e00e55c50603f8379c35cb90eaf141
saurbhcoder <saurbhcoder@gmail.com>
2026-03-12 10:31:16 +0530

    h

--- submitpop.php
+++ submitpop.php
@@ -15,7 +15,7 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->Host       = 'smtp.hostinger.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'customersupport@corvelsolar.com';                     //SMTP username
     $mail->Password   = 'Support!@#12';                               //SMTP password
