8930be8e7ab9af72765eb7012f26d0bd847434e6
saurbhcoder <saurbhcoder@gmail.com>
2026-03-12 10:39:53 +0530

    x

--- submitpop.php
+++ submitpop.php
@@ -1,65 +1,39 @@
 <?php
-use PHPMailer\PHPMailer\SMTP;
+
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require 'PHPMailer/PHPMailer.php';
-require 'PHPMailer/SMTP.php';
-require 'PHPMailer/Exception.php';
+//Load Composer's autoloader (created by composer, not included with PHPMailer)
+require 'vendor/autoload.php';
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-
-$name = $_POST['name'] ?? '';
-$phone = $_POST['phone'] ?? '';
-$pincode = $_POST['pincode'] ?? '';
-$bill = $_POST['bill'] ?? '';
-$source = $_POST['source_url'] ?? '';
-
+//Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
-
+//Support!@#12
 try {
-        //Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-        $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.hostinger.com';                     //Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'customersupport@corvelsolar.com';                     //SMTP username
-        $mail->Password   = 'Support!@#12';                               //SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.hostinger.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'customersupport@corvelsolar.com';                     //SMTP username
+    $mail->Password   = 'Support!@#12';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-        //Recipients
-        $mail->setFrom('customersupport@corvelsolar.com', 'Mailer');
-        $mail->addAddress('customersupport@corvelsolar.com', 'Joe User');     //Add a recipient
-    
+    //Recipients
+    $mail->setFrom('customersupport@corvelsolar.com', 'Mailer');
+    $mail->addAddress('customersupport@corvelsolar.com', 'Joe User');     //Add a recipient
+   
 
-        //Content
-        $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = 'Here is the subject';
-        $message = "
-        New Solar Enquiry
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-        Name: $name
-        Phone: $phone
-        Pincode: $pincode
-        Electricity Bill: $bill
-        Source URL: $source
-        IP Address: ".$_SERVER['REMOTE_ADDR']."
-        Date: ".date("Y-m-d H:i:s")."
-        ";
-
-        $mail->Body = $message;
-
-        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-        $mail->send();
-
-        header("Location: thank-you.php");
-        exit();
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    }
-
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
-
-?>
