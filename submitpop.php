0a320456fa3210eb737610d3b43f3d12ade4cd19
saurbhcoder <saurbhcoder@gmail.com>
2026-03-12 09:44:38 +0530

    err

--- submitpop.php
+++ submitpop.php
@@ -2,15 +2,21 @@
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 
 
+echo "SMTP: ".ini_get("SMTP")."<br>";
+echo "SMTP PORT: ".ini_get("smtp_port")."<br>";
+echo "sendmail_path: ".ini_get("sendmail_path");
 
 
-if(!mail("customersupport@corvelsolar.com","Test","Mail Test")){
-echo "Mail working";
+$result = mail("customersupport@corvelsolar.com","Test","Mail Test");
+
+if($result){
+    echo "Mail Sent";
 }else{
-echo "Mail not working";
+    echo "Mail Failed<br>";
+    var_dump(error_get_last());
 }
-
 
 ?>
