126e1a62853f330a6bb912ef3bb4116c0c89c1e5
saurbhcoder <saurbhcoder@gmail.com>
2026-03-12 09:48:09 +0530

    new

--- submitpop.php
+++ submitpop.php
@@ -10,18 +10,25 @@ echo "SMTP PORT: ".ini_get("smtp_port")."<br>";
 echo "sendmail_path: ".ini_get("sendmail_path");
 
 
+
+
+$headers = "From: Corvel Solar <customersupport@corvelsolar.com>\r\n";
+$headers .= "Reply-To: customersupport@corvelsolar.com\r\n";
+$headers .= "Return-Path: customersupport@corvelsolar.com\r\n";
+$headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+$headers .= "X-Mailer: PHP/" . phpversion();
+
 $to = "customersupport@corvelsolar.com";
 $subject = "Test Mail";
+$message = "This is test email from website.";
 
-$message = "Testing mail from website";
-
-$headers  = "From: Corvel Solar <customersupport@corvelsolar.com>\r\n";
-$headers .= "Reply-To: customersupport@corvelsolar.com\r\n";
+$headers = "From: customersupport@corvelsolar.com";
 
 if(mail($to,$subject,$message,$headers)){
-    echo "Mail sent";
+    echo "Mail Sent";
 }else{
-    echo "Mail failed";
+    echo "Mail Failed";
 }
+
 
 ?>
